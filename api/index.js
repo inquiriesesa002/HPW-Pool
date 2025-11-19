f4a9b4b5e2e54496fd03701db4c909900cf0a32b
inquiriesesa002 <inquiriesesa@gmail.com>
2025-11-19 15:38:29 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 // api/index.js - Vercel Serverless Function Entry
 const serverless = require("serverless-http");
 const express = require("express");
-const serverless = require("serverless-http");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("../config/database");
