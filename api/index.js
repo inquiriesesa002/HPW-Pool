e884ee4694b1a1838d68db7a3429a1dcdca0502c
inquiriesesa002 <inquiriesesa@gmail.com>
2025-11-22 14:48:55 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 // Vercel Serverless Function Entry Point
 // This file imports the Express app from backend/server.cjs
-
+//jijihhbjnjnkjkki
 const app = require('../backend/server.cjs');
 
 module.exports = app;
+
 
